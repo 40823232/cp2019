4f1c2de5ab94cfce22ad3e120cb9ef35e48c46fc
40823232 <sre81136@gmail.com>
2019-10-03 15:51:12 +0800

    initial add

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': '\n http://mde.tw/cp2019 \xa0 \n', 'tags': '', 'url': 'About.html'}, {'title': 'USB', 'text': '\n', 'tags': '', 'url': 'USB.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'http://mde.tw/cp2019 \xa0\xa0 \n', 'tags': '', 'url': 'About.html'}, {'title': 'USB', 'text': '', 'tags': '', 'url': 'USB.html'}, {'title': 'USB2', 'text': '\n', 'tags': '', 'url': 'USB2.html'}]};

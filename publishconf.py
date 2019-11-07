dcca4cb87c15fd586a036f5f9340642690ea5dbb
40823232 <sre81136@gmail.com>
2019-11-07 21:28:04 +0800

    add 20191031 blog

--- publishconf.py
+++ publishconf.py
@@ -30,7 +30,7 @@ DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
-DISQUS_SITENAME = "https-40823232-github-io-cp2019"
+DISQUS_SITENAME = "40823232-cp2019"
 DISQUS_DISPLAY_COUNTS = False
 #GOOGLE_ANALYTICS = ""
 

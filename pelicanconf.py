e5b06e1a1b05895b946ade43c4d6da804e361ffe
40823232 <sre81136@gmail.com>
2019-11-07 23:08:40 +0800

    add 20191107 blog

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals
 
 AUTHOR='40823232'
-SITENAME = 'CP2019 計算機程式 40823232
+SITENAME = 'CP2019 計算機程式 40823232'
 # 不要用文章所在目錄作為類別
 USE_FOLDER_AS_CATEGORY = False
 
